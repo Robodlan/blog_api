a6835d075795efee82177dfa31487bcc7c7dff3d
Robodlan <azuldevs@zohomail.com>
2026-01-17 13:24:36 -0400

    done

--- script.js
+++ script.js
@@ -1,46 +1,56 @@
-let html = []
+let postsArr = []
 const blogList = document.getElementById("blog-list")
-const btn = document.getElementById("btn")
-const blogTitle = document.getElementById("blog-title").value
-const blogBody = document.getElementById("blog-body").value
+const form = document.getElementById("form")
+const blogTitle = document.getElementById("blog-title")
+const blogBody = document.getElementById("blog-body")
 
-function renderPosts() {
-    postsArr = data.slice(0, 5)
-        console.log(postsArr)
-        
-        for (let post of postsArr) {
-            html += `
-                <h3>${post.title}</h3>
-                <p>${post.body}</p>
-                <hr />
-            `
-        }
-        blogList.innerHTML= html
+
+function renderPost() {
+    let html = ''
+    postsArr.forEach(post => {
+        html += `
+        <div class="blog-post">
+            <h2>${post.title}</h2>
+            <p>${post.body}</p>
+        </div>
+        <hr/>
+        `
+    })
+    blogList.innerHTML = html
 }
 
-fetch("https://jsonplaceholder.typicode.com/posts")  
-    .then(res => res.json())
-    .then(data => {
-        renderPosts(data)
-        
-    })
 
-    const options ={
-    method: "POST",
-    body: JSON.stringify({
-        title: blogTitle,
-        body: blogBody,
-    }),
-    headers: {
-        "Content-type": "application/json; charset=UTF-8",}
+
+ fetch('https://jsonplaceholder.typicode.com/posts')
+     .then(res => res.json())
+     .then(data => {
+      postsArr = data.slice(0, 5) 
+      renderPost()     
+     })
+
+     form.addEventListener("submit", (e)=> {
+    e.preventDefault()
+
+    const post = {
+        title: blogTitle.value,
+        body: blogBody.value
     }
 
-btn.addEventListener("click", function(e) {
-    e.preventDefault(e)
-   fetch("https://jsonplaceholder.typicode.com/posts", options)
-       .then(res => res.json())
-       .then(post => {
-           renderPosts(post)
-       })   
-})
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(post),
+        headers: {
+            'Content-type': 'application/json',
+        }
+    }
+    fetch('https://jsonplaceholder.typicode.com/posts', options)
+        .then(res => res.json())
+        .then(post => {
+            postsArr.unshift(post)
+            renderPost()
+            form.reset()
+        })
+
+
+     })
 
